96b8ff8663442af3f3795d1ad97f44ab3262b502
Héber Branco <heber.branco@uphold.com>
2022-07-14 10:03:24 +0100

    Improve testing suite support

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ import { NativeModules, Platform } from 'react-native';
 
 const { RNApplePay } = NativeModules;
 
+const defaultModule = {
+  RequestStatus: {},
+  canMakePayments: throwError,
+  complete: throwError,
+  requestPayment: throwError
+};
+
 const throwError = () => {
   throw new Error(`Apple Pay is for iOS only, use Platform.OS === 'ios'`)
 };
 
-const RNModule =
-  Platform.OS === 'ios'
-    ? RNApplePay :
-    {
-      RequestStatus: {},
-      canMakePayments: throwError,
-      complete: throwError,
-      requestPayment: throwError
-    };
+const RNModule = Platform.OS === 'ios' ? RNApplePay : defaultModule;
 
-const { RequestStatus, canMakePayments, complete, requestPayment } = RNModule;
+const { RequestStatus, canMakePayments, complete, requestPayment } = RNModule || defaultModule;
 
 export const ApplePay = { canMakePayments, complete, requestPayment };
 export const ApplePayRequestStatus = RequestStatus;
